d1cab77a7768c95a64f5f5b5bfc771613a29319c
Jang MyungSub <vastly10@gmail.com>
2020-03-16 17:23:00 +0900

    Create LoginForm, SignUp .vue files

---
+++ src/components/my/page/LoginForm.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="main">
+    <span>롯데백화점 공식 쇼핑몰</span>
+    <p>엘롯데에 오신 것을 환영합니다.</p>
+        <h2 class="member_title">로그인</h2>
+        <sui-form>
+    <sui-form-field>
+
+      <input placeholder="아이디" size="20"/>
+    </sui-form-field>
+    <sui-form-field>
+
+      <input placeholder="비밀번호" />
+    </sui-form-field>
+    <sui-form-field>
+      <sui-checkbox label="아이디 저장" />
+    </sui-form-field>
+    <sui-button type="submit">로그인</sui-button>
+  </sui-form>
+  <br>
+  <sui-button>아이디 찾기</sui-button>
+    <sui-button>비밀번호 찾기</sui-button>
+    <sui-button>회원가입</sui-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Sample"
+    }
+</script>
+
+<style scoped>
+    .main {
+        text-align: center;
+    }
+</style>
---
+++ src/components/my/page/SignUp.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="main">
+    <div>
+      <label>
+        <h2 class="signup-title">회원가입</h2>
+      </label>
+    </div>
+    <div>
+        <sui-input class="id-input" type="text" placeholder="아이디"/>
+        <br>
+        <sui-input class="email-input" type="text" placeholder="아이디"/>
+        @
+        <sui-input class="email-input" type="text" placeholder="아이디"/>
+        <sui-input class="email-input" type="text" placeholder="아이디"/>
+        <p>* 로그인 계정으로 이메일 주소가 사용됩니다.</p>
+        <sui-input class="phone-input-front" type="text"/>
+        <sui-input class="phone-input-back" type="text"/>
+    </div>
+    <sui-button class="signup-btn">회원가입</sui-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sample",
+  data() {
+    return {
+      domain: []
+    };
+  }
+};
+</script>
+
+<style scoped>
+.main {
+  margin-left: 30%;
+  margin-right: 30%;
+}
+sui-input {
+    width: 100%;
+}
+/* .form-field{
+    width: 30%;
+    margin-left: 30% !important;
+} */
+.id-input {
+    width: 100%;
+}
+.email-input{
+    display: inline-block;
+    width: 32.9%;
+}
+.phone-input-front{
+    width:30%;
+}
+.phone-input-back{
+    width:70%;
+}
+.signup-title{
+    text-align: center;
+}
+.signup-btn{
+    margin-left: 40% !important;
+}
+</style>
