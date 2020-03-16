f2daeaf5574c1b4d5dcca7dfcba0f0768d218be6
Songseail <seail379@gmail.com>
2020-03-16 17:43:44 +0900

    init

---
+++ src/components/share/Contents.vue
@@ -0,0 +1,58 @@
+<template>
+    <sui-container>
+        <div class="min-inner">
+            <div class="product-set">
+                <h3 class="section-title">WOMEN</h3>
+                <div class="item-list">
+                    아이템 나올 곳 들
+                </div>
+            </div>
+        </div>
+        <div class="min-inner">
+            <div class="product-set">
+                <h3 class="section-title">MEN</h3>
+                <div class="item-list">
+                    아이템 나올 곳 들
+                </div>
+            </div>
+        </div>
+        <div class="min-inner">
+            <div class="product-set">
+                <h3 class="section-title">LIFE</h3>
+                <div class="item-list">
+                    아이템 나올 곳 들
+                </div>
+            </div>
+        </div>
+        <div class="min-inner">
+            <div class="product-set">
+                <h3 class="section-title">공식스토어</h3>
+                <div class="item-list">
+                    아이템 나올 곳 들
+                </div>
+            </div>
+        </div>
+    </sui-container>
+</template>
+
+<script>
+
+    export default {
+        name: "Sample"
+    }
+</script>
+
+<style scoped>
+    .min-inner {
+        background-color: red;
+        margin-bottom:20px;
+    }
+    .section-title {
+        text-align:center;
+    }
+    .item-list {
+        background-color:blue;
+        height:100px;
+    }
+
+</style>
---
+++ src/components/share/page/MainPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Contents></Contents>
+        <Footer></Footer>
+    </div>
+</template>
+
+<script>
+    import Contents from '../Contents.vue'
+    import Footer from '../Footer.vue'
+
+    export default {
+        name: "Sample",
+        props: {
+
+        },
+        components: {
+            Contents,
+            Footer
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
