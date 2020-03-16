323faf0647504d1af944a9013e50be8508e17f4f
Parksanghyun <shpark@nextree.io>
2020-03-16 14:57:49 +0900

    init

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import SemanticUI from 'semantic-ui-vue';
+// import 'semantic-ui-css/semantic.min.css';
+// import '../semantic/dist/semantic.min.css';
+
+Vue.use(SemanticUI);
 
 Vue.config.productionTip = false
 
