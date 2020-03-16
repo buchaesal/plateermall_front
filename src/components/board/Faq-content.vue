82f447dcd59c5c613383578a85d1830a03aa8dbc
danbi choi <eks4116@gmail.com>
2020-03-16 17:42:56 +0900

    FAQ 메인화면

---
+++ src/components/board/Faq-content.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="sub_cont">
+        <FaqHeader></FaqHeader>
+        <FaqSearchForm></FaqSearchForm>
+    </div>
+</template>
+
+<script>
+    import FaqHeader from './Faq-header';
+    import FaqSearchForm from './Faq-search-form';
+
+    export default {
+        name: "Faq-content",
+        components:{
+            FaqHeader,
+            FaqSearchForm
+        }
+    }
+</script>
+
+<style scoped>
+.sub_cont{
+    float: left;
+    width: 996px;
+    font-size: 14px;
+}
+</style>
---
+++ src/components/board/Faq-search-form.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="search_faq-form">
+        <form>
+            <fieldset>
+            <input type="text" placeholder="자주 묻는 질문을 검색해보세요.">
+                <input type="submit" value="검색">
+            </fieldset>
+        </form>
+        <ul class="rank">
+            <li><a href="#">주문내역확인</a></li>
+            <li><a href="#">배송확인</a></li>
+            <li><a href="#">L.POINT</a></li>
+            <li><a href="#">반품접수</a></li>
+            <li><a href="#">교환접수</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Faq-search"
+    }
+</script>
+
+<style scoped>
+    .search_faq-form {
+        position: relative;
+        margin: 0 0 80px;
+        padding: 40px 58px 0;
+        border-top: 1px solid #ededed;
+    }
+    fieldset {
+        border: 0;
+        margin: 0;
+        padding: 0;
+    }
+    .search_faq-form input[type="text"] {
+        width: 100%;
+        height: 54px;
+        padding-left: 20px;
+        padding-right: 60px;
+        border: 0;
+        border-radius: 2px;
+        background-color: #f2f2f2;
+        font-size: 18px;
+    }
+    .search_faq-form input[type="submit"] {
+        position: absolute;
+        right: 82px;
+        top: 52px;
+        width: 44px;
+        height: 30px;
+        display: inline-block;
+        vertical-align: middle;
+        background-color: white;
+        background-size: 480px auto;
+        background-repeat: no-repeat;
+        background-position: -5px -84px;
+    }
+    .rank {
+        margin-top: 16px;
+    }
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+    .rank li {
+        float: left;
+        color: #666;
+    }
+    .search_faq-form .rank li:not(:first-child) {
+        margin-left: 20px;
+        padding-left: 20px;
+        position: relative;
+    }
+    .search_faq-form .rank li:not(:first-child):before {
+        content: '';
+        display: block;
+        position: absolute;
+        left: 0;
+        top: 50%;
+        width: 1px;
+        height: 15px;
+        margin-top: -8px;
+        background-color: #d8d8d8;
+    }
+    a{
+        color: inherit;
+    }
+</style>
