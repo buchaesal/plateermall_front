d804fa497f9a2b055acee6c2aadeaccb5c588475
Jang MyungSub <vastly10@gmail.com>
2020-03-17 13:41:20 +0900

    Revies LoginForm, SignUp.vue files and Create SignUpDetail.vue

--- src/components/my/page/LoginForm.vue
+++ src/components/my/page/LoginForm.vue
@@ -1,37 +1,49 @@
 <template>
-    <div class="main">
-    <span>롯데백화점 공식 쇼핑몰</span>
-    <p>엘롯데에 오신 것을 환영합니다.</p>
-        <h2 class="member_title">로그인</h2>
-        <sui-form>
-    <sui-form-field>
-
-      <input placeholder="아이디" size="20"/>
-    </sui-form-field>
-    <sui-form-field>
-
-      <input placeholder="비밀번호" />
-    </sui-form-field>
-    <sui-form-field>
-      <sui-checkbox label="아이디 저장" />
-    </sui-form-field>
-    <sui-button type="submit">로그인</sui-button>
-  </sui-form>
-  <br>
-  <sui-button>아이디 찾기</sui-button>
-    <sui-button>비밀번호 찾기</sui-button>
-    <sui-button>회원가입</sui-button>
-    </div>
+  <div class="login-main">
+    <p class="member_txt-small">
+      롯데백화점 공식 쇼핑몰
+      <br>
+      <span>엘롯데</span>에 오신 것을 환영합니다.
+    </p>
+    
+    <h2 class="member_title">로그인</h2>
+    <sui-form>
+      <sui-form-field>
+        <input placeholder="아이디" size="20" />
+      </sui-form-field>
+      <sui-form-field>
+        <input placeholder="비밀번호" />
+      </sui-form-field>
+      <sui-form-field>
+        <sui-checkbox label="아이디 저장" />
+      </sui-form-field>
+      <sui-button type="submit" width="50%">로그인</sui-button>
+      <sui-button>아이디 찾기</sui-button>
+      <sui-button>비밀번호 찾기</sui-button>
+      <sui-button>회원가입</sui-button>
+    </sui-form>
+    
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Sample"
-    }
+export default {
+  name: "Sample"
+};
 </script>
 
 <style scoped>
-    .main {
-        text-align: center;
-    }
+.login-main {
+  margin-top: 10%;
+  margin-left: 30%;
+  margin-right: 30%;
+}
+
+.member_txt-small {
+    /* margin: 0 0 12px 102px; */
+    font-size: 32px;
+    text-align: left;
+    font-weight: 500;
+    line-height: 44px;
+}
 </style>
--- src/components/my/page/SignUp.vue
+++ src/components/my/page/SignUp.vue
@@ -1,22 +1,44 @@
 <template>
-  <div class="main">
+  <div class="signup-main">
+    <div>
+      <sui-image @click="backToMain" src="https://simage.lotte.com/ellotte/images/login/newel_icon_top_logo.jpg" size="small" />
+    </div>
     <div>
       <label>
         <h2 class="signup-title">회원가입</h2>
       </label>
     </div>
+    <br>
+    <br>
     <div>
-        <sui-input class="id-input" type="text" placeholder="아이디"/>
+        <sui-input v-model="userName" class="id-input" type="text" placeholder="이름"/>
         <br>
-        <sui-input class="email-input" type="text" placeholder="아이디"/>
+        <br>
+        <sui-input v-model="userEmail" class="email-input" type="text" placeholder="이메일"/>
         @
-        <sui-input class="email-input" type="text" placeholder="아이디"/>
-        <sui-input class="email-input" type="text" placeholder="아이디"/>
+        <sui-input v-model="userEmailDomain" class="email-input" type="text" placeholder="직접입력"/>
+        <!-- <sui-input class="email-input" type="text" placeholder="아이디"/> -->
+        <sui-dropdown class="email-input"
+          placeholder="직접입력"
+          selection
+          :options="emailDomain"
+          v-model="currentEmailDomain"
+          @change="emailDomainChange"
+        />
+        <br>
+        <br>
         <p>* 로그인 계정으로 이메일 주소가 사용됩니다.</p>
-        <sui-input class="phone-input-front" type="text"/>
-        <sui-input class="phone-input-back" type="text"/>
+        <sui-dropdown class="phone-input-front"
+          placeholder="State"
+          selection
+          :options="headNumber"
+          v-model="currentHeadNumber"
+        />
+        <!-- <sui-input class="phone-input-front" type="text"/> -->
+        <sui-input class="phone-input-back" type="list"/>
     </div>
-    <sui-button class="signup-btn">회원가입</sui-button>
+    <br>
+    <sui-button @click="signUp" class="signup-btn">회원가입</sui-button>
   </div>
 </template>
 
@@ -25,39 +47,74 @@ export default {
   name: "Sample",
   data() {
     return {
-      domain: []
+      userName: '',
+      userEmail: '',
+      userEmailDomain: '',
+      currentEmailDomain: '직접입력',
+      currentHeadNumber: '010',
+      emailDomain: [
+        { text: 'gmail.com', value: 'gmail.com'},
+        { text: 'naver.com', value: 'naver.com'},
+        { text: 'hanmail.com', value: 'hanmail.com'},
+        { text: 'paran.com', value: 'paran.com'},
+        { text: 'nate.com', value: 'nate.com'},
+        { text: '직접입력', value: '직접입력'},
+      ],
+       headNumber: [
+        { text: '010', value: '010' },
+        { text: '011', value: '011' },
+        { text: '017', value: '017' },
+        { text: '016', value: '016' },
+        { text: '018', value: '018' },
+      ],
     };
+  },
+  methods: {
+    backToMain() {
+      //메인 링크
+    },
+    signUp() {
+      //
+    },
+    emailDomainChange(){
+      alert("ge")
+    },
   }
 };
 </script>
 
 <style scoped>
-.main {
+.signup-main {
+  margin-top: 3%;
   margin-left: 30%;
   margin-right: 30%;
 }
 sui-input {
     width: 100%;
 }
-/* .form-field{
-    width: 30%;
-    margin-left: 30% !important;
-} */
+input{
+  margin-right: 3px;
+}
 .id-input {
-    width: 100%;
+    width: 96%;
+    margin-right: 3px;
 }
 .email-input{
     display: inline-block;
-    width: 32.9%;
+    width: 30%;
+    margin-right: 3px;
 }
 .phone-input-front{
-    width:30%;
+    width:25.5%;
+    margin-right: 3px;
 }
 .phone-input-back{
-    width:70%;
+    width:63%;
+    margin-right: 3px;
 }
 .signup-title{
     text-align: center;
+
 }
 .signup-btn{
     margin-left: 40% !important;
