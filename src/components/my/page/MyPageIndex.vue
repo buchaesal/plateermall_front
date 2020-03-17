61e65b77368197bc46c7deaaea91587df496cd90
danbi choi <eks4116@gmail.com>
2020-03-17 15:22:38 +0900

    강경 푸시파

---
+++ src/components/my/page/MyPageIndex.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="main-page-container">
+        <Header></Header>
+        <FaqNav></FaqNav>
+        <component :is="selectedComponent"></component>
+        <Footer></Footer>
+    </div>
+</template>
+
+<script>
+    import Header from '../../share/Header';
+    import Footer from '../../share/Footer.vue';
+    import FaqNav from "../../faq/FaqNav";
+    import FaqContent from "../../faq/FaqContent";
+
+    export default {
+        name: "Main",
+        props:['selectedComponent'],
+        components:{
+            Header,
+            Footer,
+            FaqNav,
+            FaqContent
+        }
+    }
+</script>
+
+<style scoped>
+    #main-page-container {
+        height: 300%;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import MainPage from "../components/share/page/MainPage";
 import LoginForm from '../components/my/page/LoginForm';
+import MyPage from '../components/my/page/MyPageIndex';
 
 Vue.use(VueRouter);
 
 const routes = [
     {path: '/', component: MainPage},
     {path: '/login', component: LoginForm},
+    {path: '/faq', component: MyPage, props:{selectedComponent : 'FaqContent'}}
 ]
 
 export default new VueRouter({
