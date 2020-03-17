2b4185235784acc044cf8f0e2ed5da4e99bdb29e
JEONEK <jeonek@plateer.com>
2020-03-17 18:11:34 +0900

    GoToHome event 2 - jeon

--- src/components/share/BottomHeader.vue
+++ src/components/share/BottomHeader.vue
@@ -2,7 +2,7 @@
         <div class="menu_bar">
             <div class="inner_bar">
                 <div class="left_bar">
-                    <span class="item"><a>HOME</a></span>
+                    <span class="item"><a @click="goToHome">HOME</a></span>
                     <span class="item"><a>OUTLET</a></span>
                     <span class="item"><a>BEAUTY</a></span>
                     <span class="item"><a>BAG & ACC</a></span>
@@ -31,7 +31,12 @@
 
 <script>
     export default {
-        name: "BottomHeader.vue"
+        name: "BottomHeader.vue",
+        methods: {
+            goToHome(){
+                this.$router.push('/');
+            }
+        }
     }
 </script>
 
