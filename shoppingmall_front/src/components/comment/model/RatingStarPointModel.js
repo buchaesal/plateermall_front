ec698dc4757922e4c6092ff9e9e6822914e862b5
JEONEK <jeonek@plateer.com>
2020-03-19 00:22:12 +0900

    물의를 일으켜 죄송합니다..

--- shoppingmall_front/src/components/comment/model/RatingStarPointModel.js
+++
@@ -1,9 +0,0 @@
-class RatingStarPointModel{
-    customerCount= 0; //고객 수
-    starPoint= 0; //평균 별점
-
-    constructor(customerCount, starPoint){
-        this.customerCount = customerCount;
-        this.starPoint = starPoint;
-    }
-}
