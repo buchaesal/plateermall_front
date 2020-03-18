49cfc5e5d10c00df70423fd7aa2dcbf0a5ee6a22
Parksanghyun <shpark@nextree.io>
2020-03-18 15:12:28 +0900

    스토어 추가

---
+++ src/store/GoodsStore.js
@@ -0,0 +1,28 @@
+const state = {
+    goodsModels: [],
+    goodsCount: 0
+}
+
+const getters = {
+
+}
+
+const mutations = {
+    getGoodsList(state){
+        state.goodsCount = 3;
+    }
+}
+
+const actions = {
+
+}
+
+
+
+export default {
+    namespace: false,
+    state,
+    getters,
+    mutations,
+    actions
+  };
