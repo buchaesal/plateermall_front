4a5033e05cbfa0fe7a9b4edb337be479740cfde9
JEONEK <jeonek@plateer.com>
2020-03-18 11:24:03 +0900

    side banner

--- src/components/comment/ReviewList.vue
+++ src/components/comment/ReviewList.vue
@@ -20,28 +20,33 @@
                     <sui-dropdown-item>낮은평점순</sui-dropdown-item>
                     <sui-dropdown-item>사진상품평</sui-dropdown-item>
                 </sui-dropdown-menu>
-            </sui-dropdown>       
+            </sui-dropdown>
         </div>
         <br>
         <div class='review-list'>
             <sui-item-group divided>
                 <sui-item v-for='(review, index) in reviews' :key='index'>
                     <sui-item-content>
-                    <sui-item-header><sui-rating id="starAvg" :rating="review.starCount" :max-rating="5" /> {{review.option}}</sui-item-header>
-                    <sui-item-meta>
-                        <p>{{review.content}}</p>
-                        <img :src='review.photo' width='99' height='99'>
-                        <p>{{review.id}} | {{review.date}}</p>
+                        <sui-item-header>
+                            <sui-rating id="starAvg" :rating="review.starCount" :max-rating="5"/>
+                            {{review.option}}
+                        </sui-item-header>
+                        <sui-item-meta>
+                            <p>{{review.content}}</p>
+                            <img :src='review.photo' width='99' height='99'>
+                            <p>{{review.id}} | {{review.date}}</p>
 
-                        <span class='recommend-review'><sui-button size="tiny" floated="right" basic content="신고 하기"/></span>
-                        <span class='report-review'><sui-button size="tiny"  icon="thumbs up outline" floated="right" basic content="추천해요"/></span>
-                    </sui-item-meta>
-                <sui-item-description>
-            </sui-item-description>
-      </sui-item-content>
-    </sui-item>
-    
-    </sui-item-group>
+                            <span class='recommend-review'><sui-button size="tiny" floated="right" basic
+                                                                       content="신고 하기"/></span>
+                            <span class='report-review'><sui-button size="tiny" icon="thumbs up outline" floated="right"
+                                                                    basic content="추천해요"/></span>
+                        </sui-item-meta>
+                        <sui-item-description>
+                        </sui-item-description>
+                    </sui-item-content>
+                </sui-item>
+
+            </sui-item-group>
         </div>
     </div>
 
@@ -52,9 +57,9 @@
 
     export default {
         name: "Sample",
-        data(){
-            return{
-                reviews:[
+        data() {
+            return {
+                reviews: [
                     {
                         starCount: 5,
                         option: '사이즈선택:235',
@@ -79,17 +84,18 @@
 
 <style scoped>
 
-    .options{
+    .options {
         text-align: left;
         padding-left: 5%;
     }
-    .review-list{
+
+    .review-list {
         text-align: left;
         padding-top: 3%;
         padding-left: 5%;
     }
 
-    .photo-review{
+    .photo-review {
         border-top: 1px solid #ededed;
         border-bottom: 1px solid #ededed;
         margin-left: 2%;
@@ -100,7 +106,7 @@
         background-color: #fafafa;
     }
 
-    h3{
+    h3 {
         padding-top: 2%;
         padding-left: 2%;
         text-align: left;
@@ -109,7 +115,7 @@
         font-size: 18px;
     }
 
-    .review-img{
+    .review-img {
         float: left;
         display: inline-block;
         padding: 2%;
