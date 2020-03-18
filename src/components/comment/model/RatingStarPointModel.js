3a7f55bc4822999b8fa239dfb70f7ae30ad22461
nayunjoo <nayj124@plateer.com>
2020-03-18 11:05:29 +0900

    model update

---
+++ src/components/comment/model/RatingStarPointModel.js
@@ -0,0 +1,9 @@
+class RatingStarPointModel{
+    customerCount= 0;
+    starPoint= 0;
+
+    constructor(customerCount, starPoint){
+        this.customerCount = customerCount;
+        this.starPoint = starPoint;
+    }
+}
