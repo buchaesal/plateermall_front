3bbd93fd53655868835477e44757b02b8d0886ee
danbi choi <eks4116@gmail.com>
2020-03-19 16:19:46 +0900

    faq api

--- src/api/axios/index.js
+++ src/api/axios/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const GOODS_URL = process.env.VUE_APP_BASE_URL + '/api/goods';
 export const COMMENTS_URL = process.env.COMMENTS_APP_BASE_URL + '/api/comments';
+export const FAQ_URL = process.env.VUE_APP_BASE_URL+'/api/faq';
 
 
 const instance = axios.create({
