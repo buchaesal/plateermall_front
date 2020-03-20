b2e4825141bbd8a50dd4dedb808387572aa9a640
nayunjoo <nayj124@plateer.com>
2020-03-20 09:40:55 +0900

    api model 수정

--- src/api/model/SubCommentApiModel.js
+++ src/api/model/SubCommentApiModel.js
@@ -1,9 +1,15 @@
 class SubCommentApiModel{
     
     reviewCode='';
+    goodsCode='';
     userId='';
-
-
+    brand='';
+    itemName='';
+    option='';
+    photo='';
+    purchaseDate='';
+    dueDate='';
+    written=''; //작성여부
 }
 
 export default SubCommentApiModel;
