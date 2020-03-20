764b0f932b1fa43abe0a32d9ddd03068e3388061
JEONEK <jeonek@plateer.com>
2020-03-20 13:22:22 +0900

    Order list ....

--- src/components/my/OrderList.vue
+++ src/components/my/OrderList.vue
@@ -41,70 +41,12 @@
         </div>
         <hr>
 
-        <div>
-            <div v-for="(cart, index) in sampleData" v-bind:key="index" class="goods-list">
-                <div style="background-color:#ededed; height:50px;">
-                    <p style="text-align:left; line-height:50px; margin-right:10px;">자세히보기 ></p>
-                </div>
-                <div>
-                    <sui-grid :columns="5">
-                        <sui-grid-row stretched class="cart-grid-row">
-                            <sui-grid-column style="width:10%;">
-                                <sui-segment style="position:absolute; top:50%;">
-                                    <sui-checkbox class="goods-checkbox" :id="cart.cartCode" :value="cart.cartCode"
-                                                  v-model="checkedIndexList"/>
-                                </sui-segment>
-                            </sui-grid-column>
-                            <sui-grid-column style="width:20%;">
-                                <sui-segment>
-                                    <sui-image :src="cart.imgUrl" size="small" class="cart-img"/>
-                                </sui-segment>
-                            </sui-grid-column>
-                            <sui-grid-column style="width:40%;">
-                                <sui-segment>
-                                    <p>
-                                        {{cart.seller}}
-                                    </p>
-                                    <p>
-                                        {{cart.title}}
-                                    </p>
-                                </sui-segment>
-                            </sui-grid-column>
-                            <sui-grid-column style="width:15%;">
-                                <sui-segment>
-                                    <div class="quantity-box">
-                                        <sui-button class="minus">-</sui-button>
-                                        <sui-input :value="cart.cartStock" style="margin-left:18px;"/>
-                                        <sui-button class="plus">+</sui-button>
-                                    </div>
-                                </sui-segment>
-                                <sui-segment>
-                                    <div>
-                                        <sui-button basic content="변경"
-                                                    style="width:100px; margin-top:15px; margin-left:-15px;"/>
-                                    </div>
-                                </sui-segment>
-                            </sui-grid-column>
-                            <sui-grid-column style="width:15%; padding-bottom: 6%;">
-                                <sui-segment>
-                                    <div style="text-align:center;"><a href="#">X</a></div>
-                                </sui-segment>
-                                <sui-segment>
-                                    <div><span class="goods-price">{{priceFormatting(cart.originalPrice)}}원</span></div>
-                                </sui-segment>
-                            </sui-grid-column>
-                        </sui-grid-row>
-                    </sui-grid>
-                </div>
-            </div>
-        </div>
-
         <div class="my-order-list">
             <div v-for="(cart, index) in sampleData" v-bind:key="index" class="goods-list">
 
-                <div class="my-order-list-title">
+                <div class="my-order-list-title;" style="background-color: GhostWhite; display: inline-block; padding: 10px 30px; width: 100%;">
                     <p style="float:left;">날짜넣기</p>
-                    <a href="#" style="float:right;">자세히보기 ></a>
+                    <a href="#" style="float: right">자세히보기 ></a>
                 </div>
 
                 <div class="my-order-list-goods" style="margin: 20px 0; background-color: white; font-size: 0.8rem;">
@@ -151,6 +93,20 @@
                         originalPrice: 1206000,
                         dcRate: 3.5,
                         saleCnt: 5
+                    },
+                    {
+                        cartCode: "code1",
+                        userId: "1",
+                        cartStock: 5,
+                        imgUrl: "https://image.ellotte.com/ellt.static.lotteeps.com/goods/img/71/17/50/01/12/1201501771_mast.jpg/chg/resize/160x160/extent/160x160/optimize",
+                        goodsCode: "123",
+                        seller: "판매자1",
+                        title: "필립스(아울렛)\n" +
+                            "필립스 소닉케어 다이아몬드 클린 매트화이트 HX9338/04\n" +
+                            "모델명:HX9338/04",
+                        originalPrice: 1206000,
+                        dcRate: 3.5,
+                        saleCnt: 5
                     }
                 ],
             }
@@ -181,17 +137,14 @@
     }
 
     .my-order-list-title {
-        height: 50px;
-        padding: 0 30px;
         background-color: GhostWhite;
+        display: inline-block;
+        padding: 10px 30px;
+        width: 100%;
     }
 
-    .my-order-list-title > p {
-        margin-top: 15px;
-    }
-
-    .my-order-list-title > a {
-        margin-top: 15px;
+    .my-order-list-title p, .my-order-list-title a  {
+        margin-top: 7px;
         color: black;
     }
 
