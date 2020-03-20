e1c557b9adb97010f97001b8ffd0427abf19b1f8
JEONEK <jeonek@plateer.com>
2020-03-20 14:34:11 +0900

    Order list ....

--- src/components/my/OrderList.vue
+++ src/components/my/OrderList.vue
@@ -39,35 +39,42 @@
                 </li>
             </ul>
         </div>
+
         <hr>
 
         <div class="my-order-list">
-            <div v-for="(cart, index) in sampleData" v-bind:key="index" class="goods-list">
-
-                <div class="my-order-list-title;" style="background-color: GhostWhite; display: inline-block; padding: 10px 30px; width: 100%;">
-                    <p style="float:left;">날짜넣기</p>
-                    <a href="#" style="float: right">자세히보기 ></a>
-                </div>
-
-                <div class="my-order-list-goods" style="margin: 20px 0; background-color: white; font-size: 0.8rem;">
-                        <sui-checkbox class="goods-checkbox" style="margin: 0px 30px;"/>
-                    <span class="goods-img" style="vertical-align: middle;"><img :src="cart.imgUrl"
-                                                                                 style="width: 150px; height: auto"></span>
-                    <div style="display: inline-block; vertical-align: middle; margin-left: 40px;">
-                        <p>{{cart.seller}}</p>
-                        <p>{{cart.title}}</p>
-                        <p>수량 가져오기</p>
-                        <p>주문 진행상태 보여주기</p>
+            <div v-if="sampleData.length<1" class="no-order">
+                <sui-icon name="info" size="huge" circular color="grey"/>
+                <p class="no-order-msg">데이터가 없습니다.</p>
+            </div>
+            <div v-else>
+                <div v-for="(cart, index) in sampleData" v-bind:key="index" class="goods-list">
+                    <div class="my-order-list-title">
+                        <p class="order-date">날짜넣기</p>
+                        <a href="#" class="order-detail">자세히보기 ></a>
                     </div>
-                    <span class="goods-price" style="margin: 0 50px">{{priceFormatting(cart.originalPrice)}}원</span>
-                    <div style="display: inline-block; margin-left: 10px; text-align: center; vertical-align: middle">
-                        <button style="display: block;">배송지변경</button>
-                        <button>주문취소</button>
+
+                    <div class="my-order-list-goods">
+                        <sui-checkbox class="goods-checkbox"/>
+                        <span class="goods-img">
+                        <img :src="cart.imgUrl">
+                    </span>
+                        <div class="my-order-list-info">
+                            <p>{{cart.seller}}</p>
+                            <p>{{cart.title}}</p>
+                            <p>수량 가져오기</p>
+                            <p>주문 진행상태 보여주기</p>
+                        </div>
+                        <span class="my-order-list-price">{{priceFormatting(cart.originalPrice)}}원</span>
+                        <div class="my-order-list-button">
+                            <button class="btn1">배송지변경</button>
+                            <button>주문취소</button>
+                        </div>
                     </div>
+                    <hr>
                 </div>
             </div>
         </div>
-        <hr>
     </div>
 </template>
 
@@ -80,34 +87,35 @@
             return {
                 checkedIndexList: [],
                 sampleData: [
-                    {
-                        cartCode: "code1",
-                        userId: "1",
-                        cartStock: 5,
-                        imgUrl: "https://image.ellotte.com/ellt.static.lotteeps.com/goods/img/71/17/50/01/12/1201501771_mast.jpg/chg/resize/160x160/extent/160x160/optimize",
-                        goodsCode: "123",
-                        seller: "판매자1",
-                        title: "필립스(아울렛)\n" +
-                            "필립스 소닉케어 다이아몬드 클린 매트화이트 HX9338/04\n" +
-                            "모델명:HX9338/04",
-                        originalPrice: 1206000,
-                        dcRate: 3.5,
-                        saleCnt: 5
-                    },
-                    {
-                        cartCode: "code1",
-                        userId: "1",
-                        cartStock: 5,
-                        imgUrl: "https://image.ellotte.com/ellt.static.lotteeps.com/goods/img/71/17/50/01/12/1201501771_mast.jpg/chg/resize/160x160/extent/160x160/optimize",
-                        goodsCode: "123",
-                        seller: "판매자1",
-                        title: "필립스(아울렛)\n" +
-                            "필립스 소닉케어 다이아몬드 클린 매트화이트 HX9338/04\n" +
-                            "모델명:HX9338/04",
-                        originalPrice: 1206000,
-                        dcRate: 3.5,
-                        saleCnt: 5
-                    }
+                    // {
+                    //     cartCode: "code1",
+                    //     userId: "1",
+                    //     cartStock: 5,
+                    //     imgUrl: "https://image.ellotte.com/ellt.static.lotteeps.com/goods/img/71/17/50/01/12/1201501771_mast.jpg/chg/resize/160x160/extent/160x160/optimize",
+                    //     goodsCode: "123",
+                    //     seller: "판매자1",
+                    //     title: "필립스(아울렛)\n" +
+                    //         "필립스 소닉케어 다이아몬드 클린 매트화이트 HX9338/04\n" +
+                    //         "모델명:HX9338/04",
+                    //     originalPrice: 1206000,
+                    //     dcRate: 3.5,
+                    //     saleCnt: 5
+                    // }
+                    // ,
+                    // {
+                    //     cartCode: "code1",
+                    //     userId: "1",
+                    //     cartStock: 5,
+                    //     imgUrl: "https://image.ellotte.com/ellt.static.lotteeps.com/goods/img/71/17/50/01/12/1201501771_mast.jpg/chg/resize/160x160/extent/160x160/optimize",
+                    //     goodsCode: "123",
+                    //     seller: "판매자1",
+                    //     title: "필립스(아울렛)\n" +
+                    //         "필립스 소닉케어 다이아몬드 클린 매트화이트 HX9338/04\n" +
+                    //         "모델명:HX9338/04",
+                    //     originalPrice: 1206000,
+                    //     dcRate: 3.5,
+                    //     saleCnt: 5
+                    // }
                 ],
             }
         },
@@ -136,18 +144,6 @@
         margin: 20px 0;
     }
 
-    .my-order-list-title {
-        background-color: GhostWhite;
-        display: inline-block;
-        padding: 10px 30px;
-        width: 100%;
-    }
-
-    .my-order-list-title p, .my-order-list-title a  {
-        margin-top: 7px;
-        color: black;
-    }
-
     .total-order-box {
         list-style: none;
         padding: 0;
@@ -172,9 +168,76 @@
         margin-top: 15px;
     }
 
+    .no-order {
+        width: 100%;
+        text-align: center;
+        margin-top: 100px;
+    }
+
+    .no-order-msg {
+        margin-top: 50px;
+        font-width: bold;
+    }
+
+    .my-order-list-title {
+        background-color: GhostWhite;
+        display: inline-block;
+        padding: 8px 30px;
+        width: 100%;
+    }
+
+    .order-date {
+        display: inline-block;
+        float: left;
+        margin-top: 9px;
+        color: black;
+    }
+
+    .order-detail {
+        float: right;
+        margin-top: 9px;
+        color: black;
+    }
+
+    .my-order-list-goods {
+        margin: 20px 0;
+        background-color: white;
+        font-size: 0.8rem;
+    }
 
     .goods-checkbox {
         float: left;
+        margin: 60px 30px 0 30px;
+    }
+
+    .goods-img {
+        vertical-align: middle;
+    }
+
+    .goods-img img {
+        width: 150px;
+        height: auto
+    }
+
+    .my-order-list-info {
+        display: inline-block;
+        vertical-align: middle;
+        margin-left: 40px;
+    }
+
+    .my-order-list-price {
+        margin: 0 50px;
+    }
+
+    .my-order-list-button {
+        display: inline-block;
+        margin-left: 10px;
+        text-align: center;
+        vertical-align: middle
+    }
+
+    .btn1 {
+        display: block;
     }
 
     button {
