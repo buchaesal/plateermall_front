32b63efe47bd0ea70947789a762dc5d3fea8f5d9
danbi choi <eks4116@gmail.com>
2020-03-20 15:54:59 +0900

    하호호

--- src/components/my/Wishlist.vue
+++ src/components/my/Wishlist.vue
@@ -10,8 +10,9 @@
         <div v-if="active=='상품'">
             <WishlistDetail/>
         </div>
-        <div v-if="active=='브랜드'">
-        
+        <div v-if="active=='브랜드'" class="no-order">
+            <sui-icon name="info" size="huge" circular color="grey"/>
+            <p class="no-order-msg">데이터가 없습니다.</p>
         </div>
     </div>
 </template>
@@ -55,5 +56,15 @@ header {
     padding-left: 22% !important;
     padding-right: 25% !important;
 }
+.no-order {
+    width: 100%;
+    text-align: center;
+    margin-top: 100px;
+}
 
+.no-order-msg {
+    margin-top: 50px;
+    font-weight: bold;
+    color: gray;
+}
 </style>
