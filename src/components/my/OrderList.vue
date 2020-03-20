32fce2566c21fb740ea00c853b86ec37a1fee8e9
JEONEK <jeonek@plateer.com>
2020-03-20 14:44:23 +0900

    Order list ....

--- src/components/my/OrderList.vue
+++ src/components/my/OrderList.vue
@@ -87,35 +87,35 @@
             return {
                 checkedIndexList: [],
                 sampleData: [
-                    // {
-                    //     cartCode: "code1",
-                    //     userId: "1",
-                    //     cartStock: 5,
-                    //     imgUrl: "https://image.ellotte.com/ellt.static.lotteeps.com/goods/img/71/17/50/01/12/1201501771_mast.jpg/chg/resize/160x160/extent/160x160/optimize",
-                    //     goodsCode: "123",
-                    //     seller: "판매자1",
-                    //     title: "필립스(아울렛)\n" +
-                    //         "필립스 소닉케어 다이아몬드 클린 매트화이트 HX9338/04\n" +
-                    //         "모델명:HX9338/04",
-                    //     originalPrice: 1206000,
-                    //     dcRate: 3.5,
-                    //     saleCnt: 5
-                    // }
-                    // ,
-                    // {
-                    //     cartCode: "code1",
-                    //     userId: "1",
-                    //     cartStock: 5,
-                    //     imgUrl: "https://image.ellotte.com/ellt.static.lotteeps.com/goods/img/71/17/50/01/12/1201501771_mast.jpg/chg/resize/160x160/extent/160x160/optimize",
-                    //     goodsCode: "123",
-                    //     seller: "판매자1",
-                    //     title: "필립스(아울렛)\n" +
-                    //         "필립스 소닉케어 다이아몬드 클린 매트화이트 HX9338/04\n" +
-                    //         "모델명:HX9338/04",
-                    //     originalPrice: 1206000,
-                    //     dcRate: 3.5,
-                    //     saleCnt: 5
-                    // }
+                    {
+                        cartCode: "code1",
+                        userId: "1",
+                        cartStock: 5,
+                        imgUrl: "https://image.ellotte.com/ellt.static.lotteeps.com/goods/img/71/17/50/01/12/1201501771_mast.jpg/chg/resize/160x160/extent/160x160/optimize",
+                        goodsCode: "123",
+                        seller: "판매자1",
+                        title: "필립스(아울렛)\n" +
+                            "필립스 소닉케어 다이아몬드 클린 매트화이트 HX9338/04\n" +
+                            "모델명:HX9338/04",
+                        originalPrice: 1206000,
+                        dcRate: 3.5,
+                        saleCnt: 5
+                    }
+                    ,
+                    {
+                        cartCode: "code1",
+                        userId: "1",
+                        cartStock: 5,
+                        imgUrl: "https://image.ellotte.com/ellt.static.lotteeps.com/goods/img/71/17/50/01/12/1201501771_mast.jpg/chg/resize/160x160/extent/160x160/optimize",
+                        goodsCode: "123",
+                        seller: "판매자1",
+                        title: "필립스(아울렛)\n" +
+                            "필립스 소닉케어 다이아몬드 클린 매트화이트 HX9338/04\n" +
+                            "모델명:HX9338/04",
+                        originalPrice: 1206000,
+                        dcRate: 3.5,
+                        saleCnt: 5
+                    }
                 ],
             }
         },
@@ -176,7 +176,8 @@
 
     .no-order-msg {
         margin-top: 50px;
-        font-width: bold;
+        font-weight: bold;
+        color: gray;
     }
 
     .my-order-list-title {
