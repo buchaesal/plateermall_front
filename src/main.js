20e244207a4aa76540b0702d132af761bf00edf3
leesk9719 <leesk9719@naver.com>
2020-03-23 10:36:46 +0900

    뷰티파이ㅂㅂ

--- src/main.js
+++ src/main.js
@@ -1,22 +1,19 @@
 import Vue from 'vue';
 import App from './App.vue';
-// import Vuetify from "vuetify";
-// import 'vuetify/dist/vuetify.min.css';
 import SemanticUI from 'semantic-ui-vue';
 import router from './router';
 import store from './store';
 import VueDaumPostCode from 'vue-daum-postcode';
-import vuetify from './plugin/vuetify' // path to vuetify export
+// import vuetify from './plugin/vuetify' // path to vuetify export
 
 Vue.use(SemanticUI);
 Vue.use(VueDaumPostCode);
-// Vue.use(Vuetify);
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  vuetify,
+  // vuetify,
   render: h => h(App),
 }).$mount('#app')
