baf3ea1bbb7f510a7f67eb97677107728152e15f
vastly10 <vastly10@gmail.com>
2020-03-24 23:00:06 +0900

    배송지 버그픽스. 기능 안되는거 있으면 문의해주세요..

--- src/components/my/FindAddressPage.vue
+++ src/components/my/FindAddressPage.vue
@@ -7,9 +7,14 @@
 <script>
     export default {
         name: "FindAddressPage",
+        props: {
+          targetShippingSpot : Object,
+        },
         methods: {
             complete(result) {
-                this.$store.commit('setAddress', {roadAddress: result.roadAddress, zipcodeAddress: result.jibunAddress});
+                // this.$store.commit('setAddress', {roadAddress: result.roadAddress, zipcodeAddress: result.jibunAddress});
+                this.targetShippingSpot.roadAddress = result.roadAddress;
+                this.targetShippingSpot.zipcodeAddress = result.jibunAddress;
                 this.$emit('complete');
             }
         }
