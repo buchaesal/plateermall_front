86264f42a19301b364787972d5e9eaf6d4f66ad1
JEONEK <jeonek@plateer.com>
2020-03-24 15:02:38 +0900

    1:1 답변확인 boot

--- src/components/faq/InquiryAnswer.vue
+++ src/components/faq/InquiryAnswer.vue
@@ -17,25 +17,27 @@
             <sui-table single-line>
                 <sui-table-header>
                     <sui-table-row>
-                        <sui-table-header-cell>순번</sui-table-header-cell>
-                        <sui-table-header-cell>제목</sui-table-header-cell>
-                        <sui-table-header-cell>문의일</sui-table-header-cell>
-                        <sui-table-header-cell>답변일</sui-table-header-cell>
-                        <sui-table-header-cell>문의상태</sui-table-header-cell>
+<!--                        <sui-table-header-cell>순번</sui-table-header-cell>-->
+                        <sui-table-header-cell>territory</sui-table-header-cell>
+                        <sui-table-header-cell>title</sui-table-header-cell>
+                        <sui-table-header-cell>goodsCode</sui-table-header-cell>
+<!--                        <sui-table-header-cell>문의일</sui-table-header-cell>-->
+<!--                        <sui-table-header-cell>답변일</sui-table-header-cell>-->
+<!--                        <sui-table-header-cell>문의상태</sui-table-header-cell>-->
                     </sui-table-row>
                 </sui-table-header>
-                <sui-table-body v-if="answers.length === 0">
+                <sui-table-body v-if="faqList.length === 0">
                     <sui-table-row>
                         <sui-table-cell colspan="5" style="text-align:center;">문의 내역이 없습니다.</sui-table-cell>
                     </sui-table-row>
                 </sui-table-body>
                 <sui-table-body v-else>
-                    <sui-table-row v-for="post in answers" :key="post.id">
-                        <sui-table-cell>{{ post.id }}</sui-table-cell>
-                        <sui-table-cell><router-link :to="`/answer/${post.id}`">{{ post.title }}</router-link></sui-table-cell>
-                        <sui-table-cell>{{ post.questionDate }}</sui-table-cell>
-                        <sui-table-cell>{{ post.answerDate }}</sui-table-cell>
-                        <sui-table-cell>{{ post.status }}</sui-table-cell>
+                    <sui-table-row v-for="(post, index) in faqList" :key="index">
+                        <sui-table-cell>{{post.territory}}</sui-table-cell>
+                        <sui-table-cell>{{ post.title }}</sui-table-cell>
+                        <sui-table-cell>{{ post.goodsCode }}</sui-table-cell>
+<!--                        <sui-table-cell><router-link :to="`/answer/${post.id}`">{{ post.title }}</router-link></sui-table-cell>-->
+<!--                        <sui-table-cell>{{ post.answerDate }}</sui-table-cell>-->
                     </sui-table-row>
                 </sui-table-body>
             </sui-table>
@@ -45,20 +47,22 @@
 
 <script>
     import FaqHeader from "./FaqHeader";
-    import FaqApi from "../../api/FaqApi";
+    // import FaqApi from "../../api/FaqApi";
+    import {getFaqList} from "../../api/FaqApi";
 
     export default {
         name: "InquiryAnswer",
         components: {
-            FaqHeader
+            FaqHeader,
         },
         data() {
             return {
-                answers: []
+                faqList: [],
             }
         },
-        created(){
-            this.answers = new FaqApi().getFaqList();
+        async created() {
+            // this.answers = new FaqApi().getFaqList();
+            this.faqList = await getFaqList();
         },
     }
 </script>
