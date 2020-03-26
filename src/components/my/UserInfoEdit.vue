541efc6a8f9ce251ecc1097588feff5e33212cc3
JEONEK <jeonek@plateer.com>
2020-03-26 23:11:55 +0900

    push를 안하고 온사람 - 저요

--- src/components/my/UserInfoEdit.vue
+++ src/components/my/UserInfoEdit.vue
@@ -24,21 +24,38 @@
                 <input type="text" placeholder="위와 같은 비밀번호" />
             </sui-form-field>
 
-            <sui-form-fields inline>
-                <label for="fruit">Select your favorite fruit:</label>
-                <sui-form-field>
-                    <sui-checkbox radio name="fruit" label="Apples" value="1" />
-                </sui-form-field>
-                <sui-form-field>
-                    <sui-checkbox radio name="fruit" label="Oranges" value="2" />
-                </sui-form-field>
-                <sui-form-field>
-                    <sui-checkbox radio name="fruit" label="Pears" value="3" />
-                </sui-form-field>
-                <sui-form-field>
-                    <sui-checkbox radio name="fruit" label="Grapefruit" value="4" />
-                </sui-form-field>
-            </sui-form-fields>
+            <sui-form>
+                <sui-form-fields inline>
+                    <label for="fruit">Select your favorite fruit:</label>
+                    <sui-form-field>
+                        <sui-checkbox radio name="fruit" label="Apples" value="1" />
+                    </sui-form-field>
+                    <sui-form-field>
+                        <sui-checkbox radio name="fruit" label="Oranges" value="2" />
+                    </sui-form-field>
+                    <sui-form-field>
+                        <sui-checkbox radio name="fruit" label="Pears" value="3" />
+                    </sui-form-field>
+                    <sui-form-field>
+                        <sui-checkbox radio name="fruit" label="Grapefruit" value="4" />
+                    </sui-form-field>
+                </sui-form-fields>
+                <sui-form-fields grouped>
+                    <label for="second_fruit">Select your second favorite fruit:</label>
+                    <sui-form-field>
+                        <sui-checkbox radio name="second_fruit" label="Apples" value="1" />
+                    </sui-form-field>
+                    <sui-form-field>
+                        <sui-checkbox radio name="second_fruit" label="Oranges" value="2" />
+                    </sui-form-field>
+                    <sui-form-field>
+                        <sui-checkbox radio name="second_fruit" label="Pears" value="3" />
+                    </sui-form-field>
+                    <sui-form-field>
+                        <sui-checkbox radio name="second_fruit" label="Grapefruit" value="4" />
+                    </sui-form-field>
+                </sui-form-fields>
+            </sui-form>
 
             <sui-form-field inline>
                 <sui-form-field>
