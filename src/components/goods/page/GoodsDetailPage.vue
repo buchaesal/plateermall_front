ecb827a68883dddf93d4efad18a9790a37fc5013
leesk9719 <leesk9719@gmail.com>
2020-03-30 16:04:49 +0900

    위시리스트 추가

--- src/components/goods/page/GoodsDetailPage.vue
+++ src/components/goods/page/GoodsDetailPage.vue
@@ -413,6 +413,7 @@
     import RatingGraph from "../../comment/RatingGraph";
     import ReviewList from "../../comment/ReviewList";
     import {requestAddCart} from "../../../api/CartListApi";
+    import WishListApi from "../../../api/WishListApi";
 
     export default {
         name: "GoodsDetail",
@@ -565,6 +566,20 @@
             },
             likeBtnClick() {
                 this.isLike = !this.isLike;
+
+                let wishListApi = new WishListApi();
+
+                if (this.isLike) {
+                    wishListApi.addGoodsWish({
+                        "goodsCode": this.$route.params.goodsCode
+                    });
+
+                    alert("위시리스트에 담겼습니다.")
+                } else {
+                    wishListApi.deleteGoodsWish({
+                        "goodsCode": this.$route.params.goodsCode
+                    });
+                }
             },
             onTooltip1() {
                 this.tooltip1Display = true;
