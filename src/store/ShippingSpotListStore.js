9a52f8fd676250aa3c9c308a1683b6463ed13bc9
danbi choi <eks4116@gmail.com>
2020-03-30 18:45:47 +0900

    배송지 하는 중..

--- src/store/ShippingSpotListStore.js
+++ src/store/ShippingSpotListStore.js
@@ -1,5 +1,5 @@
-import ShippingSpotListApi from "../api/ShippingSpotListApi"
-import {addDeliveryAddress} from "../api/ShippingSpotListApi";
+// import ShippingSpotListApi from "../api/ShippingSpotListApi"
+import {addDeliveryAddress, getShippingSpotList} from "../api/ShippingSpotListApi";
 
 const state = {
     shippingSpotList: [],
@@ -10,41 +10,46 @@ const state = {
     modifyShippingSPotForm: {},
 }
 
-const getters = {
-
-}
+const getters = {}
 
 const mutations = {
-    updateShippingSpotList(state, list){
+    updateShippingSpotList(state, list) {
         state.shippingSpotList = list;
     },
-    getShippingSpotListFromApi(state){
-        state.shippingSpotList = new ShippingSpotListApi().getShippingSpotList();
-        // state.shippingSpotList = ShippingSpotListApi.getShippingSpotList();
+    getShippingSpotListFromApi(state, list) {
+        //state.shippingSpotList = new ShippingSpotListApi().getShippingSpotList();
+        state.shippingSpotList = list;
     },
     addShippingSpotList(state, shippingSpot) {
         state.shippingSpotList.push(shippingSpot);
     },
-    setAddress(state, payload){
+    setAddress(state, payload) {
         state.roadAddress = payload.roadAddress;
         state.zipcodeAddress = payload.zipcodeAddress;
     },
-    cleanAddress(state){
+    cleanAddress(state) {
         state.roadAddress = '';
         state.zipcodeAddress = '';
     }
 }
 
 const actions = {
-    getShippingSpotListFromApi(context){
-        context.commit('getShippingSpotListFromApi')
-        return new Promise(() => {
-            context.commit('getShippingSpotListFromApi');
-        })
+    async getShippingSpotListFromApi(context) {
+        let list = await getShippingSpotList()
+            .then((response) => {
+                    context.commit('getShippingSpotListFromApi', response.data);
+                    return response.data
+                }
+            ).catch(function (error) {
+                console.log(error);
+            });
+
+        console.log(list);
+        return list;
     },
-    async addShippingSpotListFromApi(context, address){
+    async addShippingSpotListFromApi(context, address) {
         await addDeliveryAddress(address);
-        context.commit('addShippingSpotList',address);
+        context.commit('addShippingSpotList', address);
     }
 }
 
