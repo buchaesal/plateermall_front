0e30af01ef064bb858787cd6b4c678decb921e85
danbi choi <eks4116@gmail.com>
2020-03-31 11:08:04 +0900

    ip바꿈

--- src/api/axios/index.js
+++ src/api/axios/index.js
@@ -11,7 +11,7 @@ export const FAQ_URL = 'http://192.168.0.17:9999/api/faq';
 export const ORDER_URL = 'http://192.168.0.228:9999/api/order';
 export const PURCHASEHISTORY_URL = process.env.PURCHASEHISTORY_APP_BASE_URL + 'api/purchasehistory';
 
-export const USER_URL = process.env.VUE_APP_BASE_URL + '/api/user';
+export const USER_URL =  'http://192.168.0.95:9999/api/user';
 export const SHIPPINGSPOT_URL = process.env.VUE_APP_BASE_URL + '/api/shippingspot';
 export const WISHLIST_URL = 'http://192.168.0.199:9999/api/wishlist';
 
