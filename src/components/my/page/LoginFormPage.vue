51800d8b835b529f9f37e21aa37a1be597bcbcca
danbi choi <eks4116@gmail.com>
2020-04-02 13:24:40 +0900

    배송지관리 기본배송지문제 수정

--- src/components/my/page/LoginFormPage.vue
+++ src/components/my/page/LoginFormPage.vue
@@ -21,7 +21,7 @@
         <p class="btns-wrap">
             <button type="button" class="loginBtn" @click="login" style="cursor: pointer">로그인</button>
         </p>
-        <sui-checkbox label="아이디 저장" value="true" v-model="saveId"/>
+        <sui-checkbox label="아이디 저장" value="true" :checked="saveId"/>
         <router-link to="/signUp" class="requestReg">회원가입</router-link>
         <router-link to="/" class="requestReg">메인으로</router-link>
     </div>
@@ -36,26 +36,27 @@
                     email: '',
                     password: ''
                 },
-                saveId: 'false'
+                saveId: false
             }
         },
         methods: {
             login() {
-                this.$store.dispatch('REQUEST_LOGIN', this.user)
+                console.log(this.saveId);
+                this.$store.dispatch('REQUEST_LOGIN', {
+                    user : this.user,
+                    isSaveId: this.saveId})
                     .then((res) => {
                         if (res) {
                             alert(res);
-                        } else {
-                            if (this.saveId === 'true') {
-                                localStorage.setItem('saveId', this.user.email);
-                            }
                         }
                     })
             }
         },
         created() {
             const id = localStorage.getItem('saveId');
+
             if (id) {
+
                 this.user.email = id;
             }
         }
--- src/store/UserStore.js
+++ src/store/UserStore.js
@@ -32,21 +32,24 @@ const actions = {
     async SIGN_UP(context, user) {
         await signUp(user);
     },
-    async REQUEST_LOGIN(context, user) {
+    async REQUEST_LOGIN(context, payload) {
         try {
             let msg = '';
-            const result = await login(user);
+            const result = await login(payload.user);
             if (result === 'noExist') {
                 msg = '아이디가 존재하지 않습니다';
             } else if (result === 'incorrect') {
                 msg = '패스워드가 올바르지 않습니다.';
             } else if (result === 'failed') {
                 msg = '로그인에 실패했습니다. 아이디와 비밀번호를 다시 확인해주세요.'
-            }else
-                {
-                    setTokenInSessionStorage(result);
-                    context.commit('LOGIN');
+            } else {
+                console.log(payload.isSaveId);
+                if(payload.isSaveId === 'true'){
+                    localStorage.setItem('saveId', payload.user.email);
                 }
+                setTokenInSessionStorage(result);
+                context.commit('LOGIN');
+            }
 
             return msg;
         } catch (e) {
