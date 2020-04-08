4900d3ec78721ef71d6a92b3d76443d14016bc73
leesk9719 <leesk9719@gmail.com>
2020-04-08 17:40:44 +0900

    안쓰는 왼쪽메뉴 일단 주석....

--- src/components/share/TopHeader.vue
+++ src/components/share/TopHeader.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="top_header">
-        <div class="left_bar">
-            <a href="#">앱다운로드</a>
-            <a href="#">바로방문 ON</a>
-        </div>
+<!--        <div class="left_bar">-->
+<!--            <a href="#">앱다운로드</a>-->
+<!--            <a href="#">바로방문 ON</a>-->
+<!--        </div>-->
         <div class="right_bar">
             <a v-if="!isAuthenticated" @click="goToLoginForm" href="#">로그인</a>
             <a v-if="isAuthenticated" @click="goToMyPage" href="#">{{userInfo.name}} 님 반갑습니다.</a>
@@ -54,23 +54,23 @@
     .top_header {
         height: 50px;
     }
-    .left_bar {
-        display: inline-block;
-        float: left;
-        margin: 10px 0 0 6%;
-    }
+    /*.left_bar {*/
+    /*    display: inline-block;*/
+    /*    float: left;*/
+    /*    margin: 10px 0 0 6%;*/
+    /*}*/
 
-    .left_bar a {
-        color: #878787;
-        font-weight: bold;
-        margin: 0 7px;
-        font-size: 0.9rem;
-    }
+    /*.left_bar a {*/
+    /*    color: #878787;*/
+    /*    font-weight: bold;*/
+    /*    margin: 0 7px;*/
+    /*    font-size: 0.9rem;*/
+    /*}*/
 
     .right_bar {
         display: inline-block;
         float: right;
-        margin: 10px 6% 0 0;
+        margin: 10px 40px 0 0;
     }
 
     .right_bar a {
