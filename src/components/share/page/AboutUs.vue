fd83a1b426b55915b37d74ee45855671f7b54666
JEONEK <jeonek@plateer.com>
2020-04-10 13:09:57 +0900

    about us...

--- src/components/share/page/AboutUs.vue
+++ src/components/share/page/AboutUs.vue
@@ -3,20 +3,33 @@
         <Header></Header>
 
         <div class="aboutus-content">
+            <sui-card class="card-content-ducain">
+                <sui-image :src="ducain.imgUrl" class="card-img"/>
+                <sui-card-content>
+                    <sui-card-header>{{ducain.name}}</sui-card-header>
+                    <sui-card-meta>{{ducain.joinDate}}</sui-card-meta>
+                    <sui-card-description>{{ducain.description}}</sui-card-description>
+                </sui-card-content>
+            </sui-card>
             <div class="content-card">
-                <sui-card v-for="(newbie, index) in list" :key="index">
-                    <sui-image :src="newbie.imgUrl"/>
-                    <sui-card-content>
-                        <sui-card-header>{{newbie.name}}</sui-card-header>
-                        <sui-card-meta>{{newbie.joinDate}}</sui-card-meta>
-                        <sui-card-description>{{newbie.description}}</sui-card-description>
-                    </sui-card-content>
-                </sui-card>
+                <sui-card-group :items-per-row="3">
+                    <sui-card class="card-content" v-for="(newbie, index) in list" :key="index">
+                        <sui-image :src="newbie.imgUrl" class="card-img"/>
+                        <sui-card-content>
+                            <sui-card-header>{{newbie.name}}</sui-card-header>
+                            <sui-card-meta>{{newbie.joinDate}}</sui-card-meta>
+                            <sui-card-description>{{newbie.description}}</sui-card-description>
+                        </sui-card-content>
+                    </sui-card>
+                </sui-card-group>
             </div>
         </div>
 
         <SideBanner></SideBanner>
-        <Footer></Footer>
+
+        <div>
+            <Footer></Footer>
+        </div>
     </div>
 </template>
 
@@ -32,26 +45,51 @@
             SideBanner,
             Footer
         },
-        data(){
-            return{
-                list:[
+        data() {
+            return {
+                ducain: {
+                    imgUrl: require('../../../assets/ducain.png'),
+                    name: 'Ducain',
+                    joinDate: 'Joined in ????.??.??',
+                    description: 'WishList, Shopping Cart',
+                },
+                list: [
+
                     {
-                        imgUrl:'../../../assets/ssi.png',
-                        name:'sae il. Song',
-                        joinDate:'Joined in 2020.01.22',
-                        description:'WishList, Shopping Cart',
+                        imgUrl: require('../../../assets/ssi.png'),
+                        name: 'sae il. Song',
+                        joinDate: 'Joined in 2020.01.22',
+                        description: 'WishList, Shopping Cart',
                     },
                     {
-                        imgUrl:'../../../assets/lsk.png',
-                        name:'sun kyung. Lee',
-                        joinDate:'Joined in 2020.01.22',
-                        description:'WishList, Shopping Cart',
+                        imgUrl: require('../../../assets/lsk.png'),
+                        name: 'sun kyung. Lee',
+                        joinDate: 'Joined in 2020.01.22',
+                        description: 'WishList, Shopping Cart',
                     },
                     {
-                        imgUrl:'../../../assets/cdb.jpg',
-                        name:'dan bi. Choi',
-                        joinDate:'Joined in 2020.01.22',
-                        description:'WishList, Shopping Cart',
+                        imgUrl: require('../../../assets/cdb.png'),
+                        name: 'dan bi. Choi',
+                        joinDate: 'Joined in 2020.01.22',
+                        description: 'WishList, Shopping Cart',
+                    },
+                    {
+                        imgUrl: require('../../../assets/jms.png'),
+                        name: 'myung sub. Jang',
+                        joinDate: 'Joined in 2020.01.22',
+                        description: 'WishList, Shopping Cart',
+                    },
+                    {
+                        imgUrl: require('../../../assets/nyj.png'),
+                        name: 'yoon joo. Na',
+                        joinDate: 'Joined in 2020.01.22',
+                        description: 'WishList, Shopping Cart',
+                    },
+                    {
+                        imgUrl: require('../../../assets/jek.png'),
+                        name: 'eun koo. Jeon',
+                        joinDate: 'Joined in 2020.01.22',
+                        description: 'WishList, Shopping Cart',
                     }
                 ]
             }
@@ -63,14 +101,31 @@
 
     #aboutus-main {
         height: 300%;
-    }
-
-    .aboutus-content {
         margin-top: 230px;
     }
 
+    .aboutus-content {
+        width: 1240px;
+        max-width: 1600px;
+        min-width: 1240px;
+        margin: 0 auto;
+    }
+
     .content-card {
-        margin: 100px 0;
         display: inline;
+    }
+
+    .card-content{
+        display: inline-block;
+        margin-bottom: 50px !important;
+    }
+
+    .card-content-ducain {
+        margin: 0 auto 50px auto !important;
+    }
+
+    .card-img {
+        width: 100%;
+        height: auto;
     }
 </style>
